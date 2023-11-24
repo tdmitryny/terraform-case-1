d7e05cc045990d3a8d114e3643f43ece3688d926
Dmitry <tdmitryny@gmail.com>
2023-11-24 13:43:43 -0500

    Deployed file

--- infra/main.tf
+++ infra/main.tf
@@ -10,3 +10,28 @@ resource "azurerm_resource_group" "resource_group" {
     location = "eastus"
   
 }
+
+
+#Create a Storage Account
+resource "azurerm_storage_account" "storage_account" {
+    name = "tfstorageaccount911"
+    resource_group_name = azurerm_resource_group.resource_group.name
+    location = azurerm_resource_group.resource_group.location
+    account_tier = "Standard"
+    account_replication_type = "LRS"
+    account_kind = "StorageV2"
+
+    static_website {
+      index_document = "index.html"
+    }
+}
+#Add a file index.html
+
+resource "azurerm_storage_blob" "blob" {
+    name = "index.html"
+    storage_account_name = azurerm_storage_account.storage_account.name
+    storage_container_name = "$web"
+    type = "Block"
+    content_type = "text/html"
+    source_content = "<h1>Deployed Terraform</h1>"
+}
