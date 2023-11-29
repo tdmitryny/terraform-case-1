a3ee1e653d84a352ecd6e171bdfa59423a4444cf
Dmitry <tdmitryny@gmail.com>
2023-11-29 16:38:01 -0500

    Test finished

--- infra/main.tf
+++ infra/main.tf
@@ -1,37 +1,37 @@
 #test
 provider "azurerm" {
-    features {}
+  features {}
 
 }
 
 #Create a resoursecgroup
 resource "azurerm_resource_group" "resource_group" {
-    name = "rg-terraform-demo-911"
-    location = "eastus"
-  
+  name     = "rg-terraform-demo-911"
+  location = "eastus"
+
 }
 
 
 #Create a Storage Account
 resource "azurerm_storage_account" "storage_account" {
-    name = "tfstorageaccount911"
-    resource_group_name = azurerm_resource_group.resource_group.name
-    location = azurerm_resource_group.resource_group.location
-    account_tier = "Standard"
-    account_replication_type = "LRS"
-    account_kind = "StorageV2"
+  name                     = "tfstorageaccount911"
+  resource_group_name      = azurerm_resource_group.resource_group.name
+  location                 = azurerm_resource_group.resource_group.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
 
-    static_website {
-      index_document = "index.html"
-    }
+  static_website {
+    index_document = "index.html"
+  }
 }
 #Add a file index.html
 
 resource "azurerm_storage_blob" "blob" {
-    name = "index.html"
-    storage_account_name = azurerm_storage_account.storage_account.name
-    storage_container_name = "$web"
-    type = "Block"
-    content_type = "text/html"
-    source_content = "<h1>Deployed Terraform</h1>"
+  name                   = "index.html"
+  storage_account_name   = azurerm_storage_account.storage_account.name
+  storage_container_name = "$web"
+  type                   = "Block"
+  content_type           = "text/html"
+  source_content         = "<h1>Deployed Terraform</h1>"
 }
